a4c0a5cbbad01dcaa6c3dcdfb72511808953d192
MartinViolante <m.vioutn@gmail.com>
2022-02-03 15:58:51 -0300

    Clase 03/02/2022

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -1,9 +1,16 @@
-/*
+/*Violante Martín - DIV Z 
+Ejercicio E/S 08
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("ok");
+	var dividendo;
+	var divisor;
+
+	dividendo=117;
+	divisor=16;
+
+	alert(dividendo % divisor);
 }
